1042af7bd68a31a86c5f5750bd5f3a5ed623f418
HuskyLYL <1308951103@qq.com>
2025-10-21 13:49:45 +0800

    commit

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -3,7 +3,7 @@ import type { CommentConfig } from "../types/config";
 export const commentConfig: CommentConfig = {
   enable: true , // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
   twikoo: {
-    envId: "huskylyl.netlify.app",
+    envId: "https://huskylyl.netlify.app",
     lang: "zh-CN", // 设置 Twikoo 评论系统语言为英文
   },
 };
